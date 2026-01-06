97da7a8c6a557f50f66fe212d073e64ddab9a909
Esvensen <elliot@elliotsvensen.com>
2026-01-06 19:09:31 +0000

    Fixed homepage copy and adjusted spacing

--- src/components/Tattoo.jsx
+++ src/components/Tattoo.jsx
@@ -9,11 +9,10 @@ const Tattoo = () => {
         <div className="copy">
           <h2>Only here for you</h2>
           <p>
-            <b>Only Here For You</b> is a tattoo studio and clothing brand based
-            in Hackney Wick, East London, founded in spring 2021.
-            <b> Only Here For You</b> has been founded by tattoo artist and
-            illustrator Elliott Lane and James Cox, vocalist of post-punk band{" "}
-            <b>Crows</b>.
+            <b>Only Here For You</b> is a tattoo studio / record label / events
+            promoter and clothing brand based in Hackney Wick, East London,
+            founded in spring 2021 by tattoo artist and illustrator Elliott Lane
+            and James Cox, vocalist of post-punk band <b>Crows</b>.
           </p>
         </div>
       </section>
@@ -29,23 +28,19 @@ const Tattoo = () => {
       <section className="row text page-container">
         <div className="copy">
           <p>
-            Both moved to London from rural mid Wales with Elliott acting as the
-            artistic director for Crows and crafting their visual aesthetic.
-            Throughout their careers, they have built a strong partnership and
-            felt it was time to create a physical space and a brand that people
-            can come to, enjoy and support. They have opened OHFY, formerly The
-            Lacemakers Sweatshop where Elliott learned his craft, and the space
-            is ready to begin its next chapter.
+            Both moved to London from mid-Wales, with Elliott serving as the
+            artistic director for Crows and shaping their visual aesthetic.
+            Throughout their careers, they have built a strong creative
+            partnership and developed OHFY as both a physical space and a brand
+            that people can visit, enjoy, and support.
           </p>
           <p>
-            The shop hosts its incredible full-time resident artists and a
-            brilliant range of world class visiting guest-artists, including
-            Gordo Letters, Daniela Sagel and Konkas.
+            OHFY hosts multiple flash days, art shows, and live music events
+            throughout the year. All clothing is produced in collaboration with
+            small independent companies and artists, and each piece is released
+            in limited editions.
           </p>
-          <p>
-            All OHFY clothing is produced via small independent companies and
-            artists and are all limited edition.
-          </p>
+          <p>Only Here For You has always been, and will always be… for you.</p>
         </div>
       </section>
 
